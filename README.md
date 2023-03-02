86711f61e80bce34823f1ed6ec2b13a1a5f2d893
SOKO <tongyiti@126.com>
2023-03-03 01:44:40 +0800

    SOKO

---
+++ README.md
@@ -0,0 +1,41 @@
+
+![GitHub](https://img.shields.io/github/license/SmileBuild/ChatGPT-Web)
+![GitHub package.json version (subfolder of monorepo)](https://img.shields.io/github/package-json/v/SmileBuild/ChatGPT-Web?filename=chatgpt-web%2Fpackage.json)
+![Last Commit](https://img.shields.io/github/last-commit/SmileBuild/Chatgpt-Web)
+[![pages-build-deployment](https://github.com/SmileBuild/ChatGPT-Web/actions/workflows/pages/pages-build-deployment/badge.svg?branch=gh-pages)](https://github.com/SmileBuild/ChatGPT-Web/actions/workflows/pages/pages-build-deployment)
+![GitHub Repo stars](https://img.shields.io/github/stars/smilebuild/ChatGPT-Web)
+ ![Vue](https://img.shields.io/badge/Vue.js-35495E?logo=vue.js&logoColor=4FC08D)
+# ChatGPT-Web
+ChatGPT page with API instand of offical pages.
+
+基于ChatGPT API的ChatGPT网页版，无登录障碍，功能丰富。
+
+# Online link
+
+You can use online link for the project now.
+
+你可以点击下面的链接直接使用。
+
+[ChatGPT-Web online](https://smilebuild.github.io/ChatGPT-Web/)
+
+# Vue Project looks like
+
+![v0.2.0](https://raw.githubusercontent.com/SmileBuild/ChatGPT-Web/main/images/ChatGPT020.png)
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ chatgpt-web/src/components/Hero.vue
@@ -0,0 +1,106 @@
+<template>
+    <el-row justify="center">
+        <el-col :span="16">
+            <el-row justify="center">
+                <p class="hero-title">ChatGPT-Web {{version}} 网页版</p>
+            </el-row>
+            <el-row justify="center">
+                <el-link class="hero-subtitle">🏡https://github.com/SmileBuild/ChatGPT-Web/</el-link>
+            </el-row>
+            <el-row justify="center">
+                <el-switch v-model="theme" size="large" active-icon="Sunny" inactive-icon="Moon" style="--el-switch-on-color:#f89898;" inline-prompt />
+            </el-row>
+            <el-row justify="center">
+                <p class="desc">😊Created by Smile </p>
+            </el-row>
+            <el-row justify="center">
+                <p class="desc">🚀Easy, Fast, Everywhere</p>
+            </el-row>
+            <el-row justify="center">
+                <p class="desc">🌊Let's surf with chatGPT now.</p>
+            </el-row>
+            <el-row :gutter="20" justify="center">
+                <el-image alt="Version"
+                    src="https://img.shields.io/badge/bilibili-共享技术官-00A1D6?logo=bilibili&link=https://space.bilibili.com/34147682" />
+                <el-image alt="wechat" src="https://img.shields.io/badge/wechat-共享技术官-07C160?logo=wechat" />
+
+
+                <el-image alt="GitHub" src="https://img.shields.io/github/license/smilebuild/chatgpt-web" />
+                <el-image alt="Version"
+                    src="https://img.shields.io/github/package-json/v/SmileBuild/ChatGPT-Web?filename=chatgpt-web%2Fpackage.json" />
+                <el-image alt="Last Commit" src="https://img.shields.io/github/last-commit/SmileBuild/Chatgpt-Web" />
+                <el-image alt="build"
+                    src="https://github.com/SmileBuild/ChatGPT-Web/actions/workflows/pages/pages-build-deployment/badge.svg?branch=gh-pages" />
+                <el-image alt="build-status"
+                    src="https://github.com/SmileBuild/ChatGPT-Web/actions/workflows/pages/pages-build-deployment" />
+                <el-image alt="build-status" src="https://img.shields.io/github/stars/smilebuild/ChatGPT-Web" />
+                <el-image alt="Vue" src="https://img.shields.io/badge/Vue.js-35495E?logo=vue.js&logoColor=4FC08D" />
+
+            </el-row>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+import packageJSON from "../../package.json";
+export default {
+    
+    name: "Hero",
+    data() {
+        return {
+            theme: true,
+            version: packageJSON.version,
+        }
+    },
+    watch: {
+        theme: {
+            handler(val) {
+                console.log(val);
+                this.changeTheme(val);
+                localStorage.setItem('theme', val)
+            },
+            deep: true
+        },
+
+    },
+    mounted() {
+        if (localStorage.getItem('theme')){
+            this.theme = localStorage.getItem('theme') == "true"? true:false;            
+        }
+
+    },
+    methods: {
+        changeTheme(light) {
+
+            document.documentElement.className = light ? '' : 'dark';
+
+        }
+    }
+
+}
+</script>
+
+<style>
+.hero-title {
+    font-size: 4rem;
+    margin: 1rem;
+    font-weight: bolder;
+}
+
+.hero-subtitle {
+    font-size: 2rem;
+    margin: 1rem;
+    font-weight: bold;
+}
+
+.el-image {
+    margin: 1px;
+}
+
+.desc {
+    font-size: 1.5rem;
+    margin: 0rem 0;
+
+
+}
+</style>
